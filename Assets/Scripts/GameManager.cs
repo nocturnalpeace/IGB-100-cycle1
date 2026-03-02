9c31bc6cf2941a1571e97e590995ced5ced61852
nocturnalpeace <145471409+nocturnalpeace@users.noreply.github.com>
2026-03-02 18:02:38 +1000

    check1

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    //Singleton Setup
+    public static GameManager instance = null;
+
+    // Awake Checks - Singleton setup
+    void Awake() {
+
+        //Check if instance already exists
+        if (instance == null)
+
+            //if not, set instance to this
+            instance = this;
+
+        //If instance already exists and it's not this:
+        else if (instance != this)
+            //Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a GameManager.
+            Destroy(gameObject);
+    }
+}
